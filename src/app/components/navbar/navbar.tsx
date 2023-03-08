100db903d200b7930a34a02e316229f80098380b
Jeremy Yeatts <jyeatts@firstorion.com>
2023-03-08 00:26:39 -0600

    Removed unused images

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -8,18 +8,18 @@ import styles from './navbar.module.scss'
 // type imports
 import { TNavbar } from '@/app/models/navbar/navbar'
 
-export default function Navbar( navbarProps: TNavbar ) {
+export default function Navbar( props: TNavbar ) {
 
   useEffect(() => {
-    console.log(`navbarProps`, navbarProps)
+    console.log(`navbar props`, props)
   })
 
   return (
     <nav className={styles.nav}>
-      <div className={styles.navTitle}>{navbarProps.navDisplayName}</div>
+      <div className={styles.navTitle}>{props.navDisplayName}</div>
       <ul className={styles.navUl}>
         {
-          navbarProps.navItems.map((item) => (
+          props.navItems.map((item) => (
             <li className={styles.navLi} key={item.title}>
               <a href={item.url}>{item.title}</a>
             </li>
