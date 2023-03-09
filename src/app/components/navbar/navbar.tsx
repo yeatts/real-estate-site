712b86787c6ec7b82490eabd5d4006975a3544fa
Jeremy Yeatts <jyeatts@firstorion.com>
2023-03-09 16:40:54 -0600

    Added hero banner and stuff

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,7 +1,3 @@
-"use client"
-
-import { useEffect } from 'react'
-
 // style imports
 import styles from './navbar.module.scss'
 
@@ -10,17 +6,13 @@ import { TNavbar } from '@/app/models/navbar/navbar'
 
 export default function Navbar( props: TNavbar ) {
 
-  useEffect(() => {
-    console.log(`navbar props`, props)
-  })
-
   return (
     <nav className={styles.nav}>
       <div className={styles.navTitle}>{props.navDisplayName}</div>
-      <ul className={styles.navUl}>
+      <ul className={styles.navItems}>
         {
           props.navItems.map((item) => (
-            <li className={styles.navLi} key={item.title}>
+            <li className={styles.navItem} key={item.title}>
               <a href={item.url}>{item.title}</a>
             </li>
           )) 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // next imports
-// import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image'
+// import { Inter } from 'next/font/google'
 
 // style imports
 import styles from './page.module.scss'
@@ -12,23 +12,7 @@ import { default as NavbarModel }from '@/app/models/navbar/navbar'
 // component imports
 import Navbar from '@/app/components/navbar/navbar'
 
-// other imports
-// import { useEffect, useState } from 'react'
-
 // const inter = Inter({ subsets: ['latin'] })
-
-{/* <h2 className={inter.className}>
-  Docs <span>-&gt;</span>
-</h2> */}
-
-{/* <Image
-  src="/vercel.svg"
-  alt="Vercel Logo"
-  className={styles.vercelLogo}
-  width={100}
-  height={24}
-  priority
-/> */}
 
 export default function Home() {
 
@@ -36,10 +20,22 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      <Navbar 
-        navDisplayName="Navbar Title"
-        navItems={navItems}
-      /> 
+      <header className={styles.header}>
+        <Navbar 
+          navDisplayName="Navbar Title"
+          navItems={navItems}
+        /> 
+      </header>
+      <section className={styles.section}>
+        <Image className={styles.hero} src="/landing-houses.jpg" alt="Hauses" fill priority/>
+        <section>
+          <h1>
+            {"I sell hauses. Let's make a deal."}
+          </h1>
+          
+
+        </section>
+      </section>
     </main>
   )
 }
